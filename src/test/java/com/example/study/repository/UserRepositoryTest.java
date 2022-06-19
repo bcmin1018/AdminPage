f7bfcb833338c9e0189482f8754138061d757210
bcmin1018 <bcmin1018@gmail.com>
2022-06-19 21:31:45 +0900

    CRUD

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -17,13 +18,31 @@ public class UserRepositoryTest extends StudyApplicationTests {
         User user = new User();
         user.setPassword("admin123$");
         user.setStatus("Y");
-        user.setAccount("TestUser01");
-        user.setEmail("TestUser01@gmail.com");
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
         user.setPhoneNumber("010-1111-1111");
         user.setCreatedAt(LocalDateTime.now());
         user.setCreatedBy("admin");
 
         User newUser = userRepository.save(user);
         System.out.println("newUser: "+newUser);
+    }
+    @Test
+    public void read(){
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(selectUser ->{
+            System.out.println("user : "+user);
+        });
+    }
+    @Test
+    public void update(){
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
     }
 }
